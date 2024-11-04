ac17e2d07b473dc55e31e55f8c8827caa1086df6
theodoreln <s233235@dtu.dk>
2024-11-04 10:40:48 +0100

    Parameters for DK

--- parameters.py
+++ parameters.py
@@ -12,31 +12,30 @@ class GSA_parameters :
         return sets
     
     def update_input(self, scenario_data, sample):
-        North_l = ["DK","NO","SE","FIN","EE","LV","LT"]
-        North = '|'.join(North_l)
-        South_l = ["IT","ES","PT","GR","SI","HR","AL","MT","CY","BA","ME","MK","RS"]
-        South = '|'.join(South_l)
-        East_l = ["PL","CZ","SK","RO","BG","HU",]
-        East = '|'.join(East_l)
-        West_l = ["FR","DE","NL","UK","BE","LU","AT","CH","IE" ]
-        West = '|'.join(West_l)
-        
+        North_l_PV = ["DK","NO","SE","NL", "DE4-E","DE4-N","DE4-W","FIN", "UK","EE","LV","LT","PL","BE"]
+        North_PV = '|'.join(North_l_PV)
+        South_l_PV = ["DE4-S","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
+        South_PV = '|'.join(South_l_PV)
         SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"].records
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)), "value"]*= sample["PV_LIMIT_NORTH"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)), "value"]*= sample["PV_LIMIT_SOUTH"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)), "value"]*= sample["PV_LIMIT_EAST"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)), "value"]*= sample["PV_LIMIT_WEST"]
-
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)), "value"]*= sample["ONS_LIMIT_EAST"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)), "value"]*= sample["ONS_LIMIT_WEST"]
+        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North_PV)), "value"]*= sample["PV_LIMIT_NORTH"]
+        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South_PV)), "value"]*= sample["PV_LIMIT_SOUTH"]
+    
+    
+        North_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE"]
+        North = '|'.join(North_l)
+        South_l = ["FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
+        South = '|'.join(South_l)
+        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DK")), "value"]*= sample["ONS_LIMIT_DK"]
+        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DE")), "value"]*= sample["ONS_LIMIT_DE"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)), "value"]*= sample["ONS_LIMIT_NORTH"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)), "value"]*= sample["ONS_LIMIT_SOUTH"]
     
         HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"].records
-        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(North), "value"] *= sample["DH2_DEMAND_NORTH"]
-        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(South), "value"] *= sample["DH2_DEMAND_SOUTH"]
-        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(East), "value"] *= sample["DH2_DEMAND_EAST"]
-        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(West), "value"] *= sample["DH2_DEMAND_WEST"]
+        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DK"), "value"] *= sample["DH2_DEMAND_DK"]
+        North_and_South_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
+        North_and_South = '|'.join(North_and_South_l)
+        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DE"), "value"] *= sample["DH2_DEMAND_DE"]
+        HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.startswith((North_and_South)), "value"] *= sample["DH2_DEMAND_Rest"]
 
         EMI_POL = scenario_data["EMI_POL"].records
         EMI_POL.loc[(EMI_POL["GROUP"]=="ALL_SECTORS") & (EMI_POL["EMIPOLSET"]=="TAX_CO2"), "value"] *= sample["CO2_TAX"]
@@ -64,14 +63,12 @@ class GSA_parameters :
         FUELPRICE.loc[FUELPRICE["FFF"]=="IMPORT_H2","value"] *= sample["IMPORT_H2_P"]
         
         DE = scenario_data["DE"].records
-        DE.loc[DE["RRR"].str.contains(North), "value"] *= sample["DE_DEMAND_NORTH"]
-        DE.loc[DE["RRR"].str.contains(South), "value"] *= sample["DE_DEMAND_SOUTH"]
-        DE.loc[DE["RRR"].str.contains(East), "value"] *= sample["DE_DEMAND_EAST"]
-        DE.loc[DE["RRR"].str.contains(West), "value"] *= sample["DE_DEMAND_WEST"]
+        DE.loc[DE["RRR"].str.contains("DK"), "value"] *= sample["DE_DEMAND_DK"]
+        DE.loc[DE["RRR"].str.contains("DE"), "value"] *= sample["DE_DEMAND_DE"]
+        DE.loc[~DE["RRR"].str.contains("DK|DE"), "value"] *= sample["DE_DEMAND_Rest"]
+
         
-        DE.loc[DE["RRR"].str.contains(North) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_NORTH"]
-        DE.loc[DE["RRR"].str.contains(South) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_SOUTH"]
-        DE.loc[DE["RRR"].str.contains(East) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_EAST"]
-        DE.loc[DE["RRR"].str.contains(West) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_WEST"]
+        DE.loc[DE["RRR"].str.contains("DE") & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_DE"]
+        DE.loc[~DE["RRR"].str.contains("DE") & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_REST"]
         
         return scenario_data
