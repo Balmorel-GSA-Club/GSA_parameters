f859ec1692942299fd6cd951800dd81f98aa1e58
theodoreln <s233235@dtu.dk>
2024-11-27 13:31:32 +0100

    New way of importing parameters into scenarios

--- parameters.py
+++ parameters.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from gamspy import SpecialValues
 
 # This class is used to deal with paramneters in the model
 class GSA_parameters :
@@ -17,37 +16,46 @@ class GSA_parameters :
         North_PV = '|'.join(North_l_PV)
         South_l_PV = ["DE4-S","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
         South_PV = '|'.join(South_l_PV)
-        SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"].records
+        
+        SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"]
+        SUBTECHGROUPKPOT.columns = ["CCCRRRAAA", "TECH_GROUP", "SUBTECH_GROUP", "value"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North_PV)), "value"]*= sample["PV_LIMIT_NORTH"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South_PV)), "value"]*= sample["PV_LIMIT_SOUTH"]
         
-        FUELPRICE = scenario_data["FUELPRICE"].records
+        FUELPRICE = scenario_data["FUELPRICE"]
+        FUELPRICE.columns = ["YYY", "AAA", "FFF", "value"]
         FUELPRICE.loc[FUELPRICE["FFF"]=="NATGAS","value"]*=sample["NATGAS_P"]
     
         North_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE"]
         North = '|'.join(North_l)
         South_l = ["FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
         South = '|'.join(South_l)
+        
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DK")), "value"]*= sample["ONS_LIMIT_DK"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DE")), "value"]*= sample["ONS_LIMIT_DE"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)), "value"]*= sample["ONS_LIMIT_NORTH"]
         SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)), "value"]*= sample["ONS_LIMIT_SOUTH"]
     
-        HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"].records
+        HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"]
+        HYDROGEN_DH2.columns = ["YYY", "CCCRRRAAA", "value"]
         HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DK"), "value"] *= sample["DH2_DEMAND_DK"]
         North_and_South_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
         North_and_South = '|'.join(North_and_South_l)
         HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DE"), "value"] *= sample["DH2_DEMAND_DE"]
         HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.startswith((North_and_South)), "value"] *= sample["DH2_DEMAND_Rest"]
 
-        EMI_POL = scenario_data["EMI_POL"].records
+        EMI_POL = scenario_data["EMI_POL"]
+        EMI_POL.columns = ["YYY", "CCCRRRAAA", "GROUP", "EMIPOLSET", "value"]
         EMI_POL.loc[(EMI_POL["GROUP"]=="ALL_SECTORS") & (EMI_POL["EMIPOLSET"]=="TAX_CO2"), "value"] *= sample["CO2_TAX"]
         
-        CCS_CO2CAPTEFF_G = scenario_data["CCS_CO2CAPTEFF_G"].records
+        CCS_CO2CAPTEFF_G = scenario_data["CCS_CO2CAPTEFF_G"]
+        CCS_CO2CAPTEFF_G.columns = ["GGG", "value"]
         CCS_CO2CAPTEFF_G.loc[:, "value"] *= sample["CO2_EFF"]
         
-        GDATA = scenario_data["GDATA_numerical"].records
-        GDATA_CAT = scenario_data["GDATA_categorical"].records
+        GDATA = scenario_data["GDATA_numerical"]
+        GDATA.columns = ["GGG", "GDATASET", "value"]
+        GDATA_CAT = scenario_data["GDATA_categorical"]
+        GDATA_CAT.columns = ["GGG", "GDATASET_categorical", "TYPES"]
         GDATA.loc[(GDATA["GGG"].str.contains("GNR_ELYS_ELEC_AEC")) & (GDATA["GDATASET"]=="GDFE"),"value"] *= sample["ELYS_ELEC_EFF"]
         GDATA.loc[(GDATA["GGG"].str.contains("GNR_ELYS_ELEC_AEC")) & (GDATA["GDATASET"]=="GDOMFCOST0"),"value"] *= sample["H2_OandM"]
         GDATA.loc[(GDATA["GGG"].str.contains("GNR_H2S_H2")) & (GDATA["GDATASET"]=="GDINVCOST0"),"value"] *= sample["H2S_INVC"]
@@ -56,21 +64,23 @@ class GSA_parameters :
         GDATA.loc[(GDATA["GGG"].isin(GDATA_CAT.loc[(GDATA_CAT["TYPES"]=="SOLARPV"), "GGG"])) & (GDATA["GDATASET"]=="GDINVCOST0"),"value"] *= sample["PV_INVC"]
         GDATA.loc[(GDATA["GGG"].isin(GDATA_CAT.loc[(GDATA_CAT["TYPES"]=="WINDTURBINE_ONSHORE"), "GGG"])) & (GDATA["GDATASET"]=="GDINVCOST0"),"value"] *= sample["ONS_WT_INVC"]
         
-        XH2INVCOST = scenario_data["XH2INVCOST"].records
+        XH2INVCOST = scenario_data["XH2INVCOST"]
+        XH2INVCOST.columns = ["YYY", "IRRE", "IRRI", "value"]
         XH2INVCOST.loc[:,"value"] *= sample["H2_TRANS_INVC"]
         
-        XINVCOST = scenario_data["XINVCOST"].records
+        XINVCOST = scenario_data["XINVCOST"]
+        XINVCOST.columns = ["YYY", "IRRE", "IRRI", "value"]
         XINVCOST.loc[:,"value"] *= sample["ELEC_TRANS_INVC"]
         
-        FUELPRICE = scenario_data["FUELPRICE"].records
+        FUELPRICE = scenario_data["FUELPRICE"]
         FUELPRICE.loc[FUELPRICE["FFF"]=="IMPORT_H2","value"] *= sample["IMPORT_H2_P"]
         
-        DE = scenario_data["DE"].records
+        DE = scenario_data["DE"]
+        DE.columns = ["YYY", "RRR", "DEUSER", "value"]
         DE.loc[DE["RRR"].str.contains("DK"), "value"] *= sample["DE_DEMAND_DK"]
         DE.loc[DE["RRR"].str.contains("DE"), "value"] *= sample["DE_DEMAND_DE"]
         DE.loc[~DE["RRR"].str.contains("DK|DE"), "value"] *= sample["DE_DEMAND_Rest"]
 
-        
         DE.loc[DE["RRR"].str.contains("DE") & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_DE"]
         DE.loc[~DE["RRR"].str.contains("DE") & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")), "value"] *= sample["TRANS_DEMAND_REST"]
         
