c8b706ad8e81cc5f526f7434205fc98789a1b99e
thbal <thbal@dtu.dk>
2026-07-14 12:25:46 +0200

    Add files via upload

--- parameters.py
+++ parameters.py
@@ -8,7 +8,9 @@ class GSA_parameters :
         self.parameters = self.input["Parameter"]
 
     def load_sets(self):
-        sets = "FUELPRICE, GDATA_numerical, GDATA_categorical, EMI_POL, CCS_CO2CAPTEFF_G, XINVCOST, XH2INVCOST, DE, SUBTECHGROUPKPOT, HYDROGEN_DH2"
+        #sets = "FUELPRICE, GDATA_numerical, GDATA_categorical, EMI_POL, CCS_CO2CAPTEFF_G, XINVCOST, XH2INVCOST, DE, SUBTECHGROUPKPOT, HYDROGEN_DH2, EV_BEV_available, BEV_TECH_DATA, DR_ADOPTIONRATE, DR_DATA"
+        sets = "FUELPRICE, GDATA_numerical, EMI_POL, CCS_CO2CAPTEFF_G, XINVCOST, XH2INVCOST, DE, SUBTECHGROUPKPOT, HYDROGEN_DH2, EV_BEV_available, BEV_TECH_DATA, DR_ADOPTIONRATE, DR_DATA"
+        
         return sets
     
     def update_input(self, scenario_data, sample):
@@ -21,49 +23,35 @@ class GSA_parameters :
         West_l = ["FR","DE","NL","UK","BE","LU","AT","CH","IE" ]
         West = '|'.join(West_l)
 
+
+
         FUELPRICE = scenario_data["FUELPRICE"]
         FUELPRICE.columns = ["YYY", "AAA", "FFF", "value"]
         FUELPRICE.loc[(FUELPRICE["FFF"]=="NATGAS") & (FUELPRICE["value"]>=1e-320),"value"]*=sample["NATGAS_P"]
         
-        SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"]
-        SUBTECHGROUPKPOT.columns = ["CCCRRRAAA", "TECH_GROUP", "SUBTECH_GROUP", "value"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["PV_LIMIT_NORTH"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["PV_LIMIT_SOUTH"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["PV_LIMIT_EAST"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["PV_LIMIT_WEST"]
-
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["ONS_LIMIT_EAST"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["ONS_LIMIT_WEST"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["ONS_LIMIT_NORTH"]
-        SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)) & (SUBTECHGROUPKPOT["value"]>=1e-320), "value"]*= sample["ONS_LIMIT_SOUTH"]
-    
-        HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"]
-        HYDROGEN_DH2.columns = ["YYY", "CCCRRRAAA", "value"]
-        HYDROGEN_DH2.loc[(HYDROGEN_DH2["CCCRRRAAA"].str.contains(North)) & (HYDROGEN_DH2["value"]>=1e-320), "value"] *= sample["DH2_DEMAND_NORTH"]
-        HYDROGEN_DH2.loc[(HYDROGEN_DH2["CCCRRRAAA"].str.contains(South)) & (HYDROGEN_DH2["value"]>=1e-320), "value"] *= sample["DH2_DEMAND_SOUTH"]
-        HYDROGEN_DH2.loc[(HYDROGEN_DH2["CCCRRRAAA"].str.contains(East)) & (HYDROGEN_DH2["value"]>=1e-320), "value"] *= sample["DH2_DEMAND_EAST"]
-        HYDROGEN_DH2.loc[(HYDROGEN_DH2["CCCRRRAAA"].str.contains(West)) & (HYDROGEN_DH2["value"]>=1e-320), "value"] *= sample["DH2_DEMAND_WEST"]
-
+      
         EMI_POL = scenario_data["EMI_POL"]
         EMI_POL.columns = ["YYY", "CCCRRRAAA", "GROUP", "EMIPOLSET", "value"]
         EMI_POL.loc[(EMI_POL["GROUP"]=="ALL_SECTORS") & (EMI_POL["EMIPOLSET"]=="TAX_CO2") & (EMI_POL["value"]>=1e-320), "value"] *= sample["CO2_TAX"]
         
-        CCS_CO2CAPTEFF_G = scenario_data["CCS_CO2CAPTEFF_G"]
-        CCS_CO2CAPTEFF_G.columns = ["GGG", "value"]
-        CCS_CO2CAPTEFF_G.loc[(CCS_CO2CAPTEFF_G["value"]>=1e-320), "value"] *= sample["CO2_EFF"]
         
         GDATA = scenario_data["GDATA_numerical"]
         GDATA.columns = ["GGG", "GDATASET", "value"]
-        GDATA_CAT = scenario_data["GDATA_categorical"]
-        GDATA_CAT.columns = ["GGG", "GDATASET_categorical", "TYPES"]
-        GDATA.loc[(GDATA["GGG"].str.contains("GNR_ELYS_ELEC_AEC")) & (GDATA["GDATASET"]=="GDFE") & (GDATA["value"]>=1e-320),"value"] *= sample["ELYS_ELEC_EFF"]
-        GDATA.loc[(GDATA["GGG"].str.contains("GNR_ELYS_ELEC_AEC")) & (GDATA["GDATASET"]=="GDOMFCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["H2_OandM"]
+        #GDATA_CAT = scenario_data["GDATA_categorical"]
+        #GDATA_CAT.columns = ["GGG", "GDATASET_categorical", "TYPES"]
+        GDATA.loc[(GDATA["GGG"].str.contains("GNR_ELYS_ELEC_AEC")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["H2_INVCOST0"]
+        GDATA.loc[(GDATA["GGG"].str.contains("PV")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["PV_INVC"]
+        GDATA.loc[(GDATA["GGG"].str.contains("ONS")) & (GDATA["GDATASET"]=="GDINVCOST0")  & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["ONS_WT_INVC"]
+        GDATA.loc[(GDATA["GGG"].str.contains("OFF")) & (GDATA["GDATASET"]=="GDINVCOST0")  & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["OFF_WT_INVC"]
         GDATA.loc[(GDATA["GGG"].str.contains("GNR_H2S_H2")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["H2S_INVC"]
-        GDATA.loc[(GDATA["GGG"].str.contains("GNR_STEAM-REFORMING-CCS")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["SMR_CCS_INVC"]
-        GDATA.loc[(GDATA["GGG"].str.contains("GNR_STEAM-REFORMING-CCS")) & (GDATA["GDATASET"]=="GDOMFCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["SMR_CCS_OandM"]
-        GDATA.loc[(GDATA["GGG"].isin(GDATA_CAT.loc[(GDATA_CAT["TYPES"]=="SOLARPV"), "GGG"])) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["PV_INVC"]
-        GDATA.loc[(GDATA["GGG"].isin(GDATA_CAT.loc[(GDATA_CAT["TYPES"]=="WINDTURBINE_ONSHORE"), "GGG"])) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["ONS_WT_INVC"]
+        GDATA.loc[(GDATA["GGG"].str.contains("GNR_HS_HEAT")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["HS_INVC"]
+        GDATA.loc[(GDATA["GGG"].str.contains("GNR_HP_ELEC")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["HP_INVC"]
         
+        #Batteries GRID Lithium
+        GDATA.loc[(GDATA["GGG"].str.contains("GNR_ES_ELEC_BAT-LITHIO-GRID")) & (GDATA["GDATASET"]=="GDOMFCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["BATTERIES_OandM"]
+        GDATA.loc[(GDATA["GGG"].str.contains("GNR_ES_ELEC_BAT-LITHIO-GRID")) & (GDATA["GDATASET"]=="GDINVCOST0") & (GDATA["value"]>=1e-320),"value"] *= sample["BATTERIES_INVCOST0"]
+        
+
         XH2INVCOST = scenario_data["XH2INVCOST"]
         XH2INVCOST.columns = ["YYY", "IRRE", "IRRI", "value"]
         XH2INVCOST.loc[(XH2INVCOST["value"]>=1e-320),"value"] *= sample["H2_TRANS_INVC"]
@@ -72,19 +60,43 @@ class GSA_parameters :
         XINVCOST.columns = ["YYY", "IRRE", "IRRI", "value"]
         XINVCOST.loc[(XINVCOST["value"]>=1e-320),"value"] *= sample["ELEC_TRANS_INVC"]
         
-        FUELPRICE = scenario_data["FUELPRICE"]
-        FUELPRICE.loc[(FUELPRICE["FFF"]=="IMPORT_H2") & (FUELPRICE["value"]>=1e-320),"value"] *= sample["IMPORT_H2_P"]
         
-        DE = scenario_data["DE"]
-        DE.columns = ["YYY", "RRR", "DEUSER", "value"]
-        DE.loc[(DE["RRR"].str.contains(North)) & (DE["value"]>=1e-320), "value"] *= sample["DE_DEMAND_NORTH"]
-        DE.loc[(DE["RRR"].str.contains(South)) & (DE["value"]>=1e-320), "value"] *= sample["DE_DEMAND_SOUTH"]
-        DE.loc[(DE["RRR"].str.contains(East)) & (DE["value"]>=1e-320), "value"] *= sample["DE_DEMAND_EAST"]
-        DE.loc[(DE["RRR"].str.contains(West)) & (DE["value"]>=1e-320), "value"] *= sample["DE_DEMAND_WEST"]
-        
-        DE.loc[DE["RRR"].str.contains(North) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")) & (DE["value"]>=1e-320), "value"] *= sample["TRANS_DEMAND_NORTH"]
-        DE.loc[DE["RRR"].str.contains(South) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")) & (DE["value"]>=1e-320), "value"] *= sample["TRANS_DEMAND_SOUTH"]
-        DE.loc[DE["RRR"].str.contains(East) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")) & (DE["value"]>=1e-320), "value"] *= sample["TRANS_DEMAND_EAST"]
-        DE.loc[DE["RRR"].str.contains(West) & (DE["DEUSER"].str.contains("TRANS_TRAINS|TRANS_BUS")) & (DE["value"]>=1e-320), "value"] *= sample["TRANS_DEMAND_WEST"]
-        
+        #EV fleet available
+        EV_BEV_AVAILABLE = scenario_data["EV_BEV_available"]
+        EV_BEV_AVAILABLE.columns = ["YYY", "RRR", "value"]
+        EV_BEV_AVAILABLE["value"]*=sample["EV_BEV_available"]
+
+        #EV V2G efficiency
+        EV_TECH_DATA = scenario_data["BEV_TECH_DATA"]
+        EV_TECH_DATA.columns = ["YYY", "TYPE", "value"]
+        EV_TECH_DATA.loc[(EV_TECH_DATA["TYPE"]=="EV_V2G_PEFF"),"value"]*=sample["V2G_EFF"]
+
+    
+
+
+        #EV Charger capacity
+        EV_TECH_DATA = scenario_data["BEV_TECH_DATA"]
+        EV_TECH_DATA.columns = ["YYY", "TYPE", "value"]
+        EV_TECH_DATA.loc[(EV_TECH_DATA["TYPE"]=="EV_CHARGE_CAP"),"value"]*=sample["EV_CHARGE_CAP"]
+
+        #EV V2G share
+        #EV Charger capacity
+        EV_TECH_DATA = scenario_data["BEV_TECH_DATA"]
+        EV_TECH_DATA.columns = ["YYY", "TYPE", "value"]
+        EV_TECH_DATA.loc[(EV_TECH_DATA["TYPE"]=="Share_EV_V2G"),"value"]*=sample["V2G_SHARE"]
+
+
+
+        #DR adoption rate
+        DR_ADOPTIONRATE = scenario_data["DR_ADOPTIONRATE"]
+        DR_ADOPTIONRATE.columns = ["DR_TECHNOLOGIES", "YYY", "value"]
+        DR_ADOPTIONRATE.loc[(DR_ADOPTIONRATE["DR_TECHNOLOGIES"]=="DR_SHIFT_HH_HEAT"),"value"]*=sample["ADOPTION_RATE_DR"]
+        DR_ADOPTIONRATE.loc[(DR_ADOPTIONRATE["DR_TECHNOLOGIES"]=="DR_SHIFT_HH_WATER"),"value"]*=sample["ADOPTION_RATE_DR"]
+
+
+        #H2 demand
+        HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"]
+        HYDROGEN_DH2.columns = ["YYY", "CCCRRRAAA", "value"]
+        HYDROGEN_DH2.loc[(HYDROGEN_DH2["value"]>=1e-320), "value"] *= sample["DH2_DEMAND"]
+                
         return scenario_data
